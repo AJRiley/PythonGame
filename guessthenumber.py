7186d37dd793b1d3319c1fdaf88de32e0cb5ba32
AJ Riley <ajriley774@gmail.com>
2017-02-18 20:56:12 +0000

    Add files via upload

---
+++ guessthenumber.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import random
+
+number = random.randint(1, 10)
+tries = 1
+
+
+uname = input("Hello, What is your username?")
+
+print("Hello", uname + ".", )
+
+question = input("Would you like to play a game? [Y/N] ")
+if question == "n":
+    print("oh..okay")
+
+if question == "y":
+    print("I'm thinking of a number between 1 & 10")
+    guess = int(input("Have a guess: "))
+    if guess > number:
+        print("Guess lower...")
+if guess < number:
+    print("Guess higher..")
+while guess != number:
+    tries += 1
+    guess = int(input("Try again: "))
+    if guess < number:
+       print("Guess Higher...")
+    if guess > number:
+       print("Guess Lower...")
+if guess == number:
+    print("You're right! you win! The number was", number, \
+          "and it only", tries, "tries!")
